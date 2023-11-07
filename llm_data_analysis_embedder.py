9363be9dc63552bd0640ea41391508ecd2a3f8d9
Riccardo Albero <albero180@gmail.com>
2023-11-07 17:02:43 +0900

    add way to select different LLMs

--- llm_data_analysis_embedder.py
+++ llm_data_analysis_embedder.py
@@ -2,14 +2,25 @@ from cat.mad_hatter.decorators import tool, hook, plugin
 from pydantic import BaseModel
 from datetime import datetime, date
 from langchain.docstore.document import Document
+import langchain
+from enum import Enum
 from cat.log import log
+from cat.db import crud
+import cat.factory.llm as llms
+
+class LLMIds(Enum):
+    OpenAIChat: str = 'LLMOpenAIChatConfig'
+    use_default_llm: bool = True,
+    LLOllama: str = 'LLMOllamaConfig'
+
 
 class MySettings(BaseModel):
     query_llm_prefix: str = "Find the relevant information that should be embedded from the text retrieved from the source in a clean way, without any comments"
-
+    use_default_llm: bool = True,
+    llm_id: LLMIds = LLMIds.OpenAIChat
 
 @plugin
-def settings_schema():   
+def settings_schema():
     return MySettings.schema()
 
 
@@ -17,9 +28,37 @@ def settings_schema():
 def before_rabbithole_splits_text(doc: Document, cat) -> Document:
 
     settings = cat.mad_hatter.plugins["LLMDAE-plugin"].load_settings()
+    log.error(settings)
+    prompt = f"{settings['query_llm_prefix']}\n from source(${doc[0].metadata['source']}) \n text: {doc[0].page_content}"
 
-    result = cat.llm(f"{settings['query_llm_prefix']}"
-            f"\n from source(${doc[0].metadata['source']})"
-            f"\n text: {doc[0].page_content}")
+    log.error(prompt)
+
+    if settings['use_default_llm']:
+        result = cat.llm(prompt)
+    else:
+        selected_llm_class = settings['llm_id']
+
+        FactoryClass = getattr(llms, selected_llm_class)
+
+        # obtain configuration and instantiate LLM
+        selected_llm_config = crud.get_setting_by_name(name=selected_llm_class)
+        try:
+            llm = FactoryClass.get_llm_from_config(selected_llm_config["value"])
+        except Exception as e:
+            import traceback
+            traceback.print_exc()
+            llm = llms.LLMDefaultConfig.get_llm_from_config({})
+
+
+        # Check if self._llm is a completion model and generate a response
+        if isinstance(llm, langchain.llms.base.BaseLLM):
+            result = llm(prompt, callbacks=[])
+
+        # Check if self._llm is a chat model and call it as a completion model
+        if isinstance(llm, langchain.chat_models.base.BaseChatModel):
+            result = llm.call_as_llm(prompt, callbacks=[])
+
+        log.error(result)
     doc[0].page_content = result
+
     return doc
